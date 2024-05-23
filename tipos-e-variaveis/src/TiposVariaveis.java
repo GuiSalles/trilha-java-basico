b69af7b7fd6f8b50239f59e206a88dcc8e1acc88
guilherme salles <guilhermexsalles@hotmail.com>
2024-05-23 14:53:04 -0300

    Commitando anotações de exemplos das aulas de tipos e operadores

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        double salarioMinimo = 2500;
+        
+        short numeroCurto = 1;
+
+        int numero = 10;
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
+
+//Os tipos primitivos em java são: int, byte, short, long, float, double, boolean e char
+//Ou seja, não são obejtos, são tipos primitivos.
+//São armazenados diretamente nas pilhas de memória
