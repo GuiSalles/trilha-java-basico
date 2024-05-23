80117f24775d04c4b0ee72261b0f8447249f367e
guilherme salles <guilhermexsalles@hotmail.com>
2024-05-23 16:42:11 -0300

    Commitando as anotações das últimas aulas sobre operadores

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -25,5 +25,42 @@ public class Operadores {
         System.out.println(numero++); //Continua imprimindo 6
         System.out.println(numero); //Imprime 7
         System.out.println(++numero); //Imprime 8
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String res = a==b?"verdadeiro" : "falso"; //Se forem iguais, imprime verdadeiro, se não, imprime falso
+        System.out.println(res);
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println(simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println(simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println(simNao);
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.print("As duas condições são verdadeiras");
+        } else {
+            System.out.print("Uma ou ambas as condições são falsas");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.print("Uma das condições é verdadeira");
+        } else {
+            System.out.print("ambas as condições são falsas");
+        }
     }
 }
